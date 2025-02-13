8a2d298754c2e134d0e94aac614e55eb67281fdb
Marudhupandiyansekar <158998973+Marudhupandiyansekar@users.noreply.github.com>
2025-02-13 13:35:05 +0530

    Update CharacterFreqString.java

--- src/org/interview/CharacterFreqString.java
+++ src/org/interview/CharacterFreqString.java
@@ -2,17 +2,17 @@ package org.interview;
 
 public class CharacterFreqString  {
     public static void main(String[] args) {
-        String s = "helloworld"; // Example input string
-        int[] count = new int[256]; // Array to store frequency of each character
+        String s = "helloworld"; 
+        int[] count = new int[256]; 
 
-        // Iterate over each character in the string
+        
         for (int i = 0; i < s.length(); i++) {
             char c=s.charAt(i);
             
             count[c]++;
         }
 
-        // Display the frequency of each character
+    
         for (int i = 0; i < 256; i++) {
             if (count[i] > 0) {
                 System.out.println((char)i + ": " + count[i]);
